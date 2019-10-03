b1c854478dc03ed712f23181a4976c416a36812c
MoonOld <431002206@qq.com>
2019-10-03 08:11:19 +0800

    已将注释补上

--- Clisock.py
+++ Clisock.py
@@ -8,25 +8,25 @@ clisocket.close()                           #关闭客户端socket
 
 
 
-from socket import *
+from socket import *                                      #import socket的everything
 
-HOST = 'localhost'
-PORT = 21567
-BUFSIZ = 1024
-ADDR = (HOST,PORT)
+HOST = 'localhost'                                        #主机名（ip
+PORT = 21567                                              #端口
+BUFSIZ = 1024                                             #设置缓冲区大小
+ADDR = (HOST,PORT)                                        #地址设定
 
-UDPClientSock = socket(AF_INET, SOCK_DGRAM)
+UDPClientSock = socket(AF_INET, SOCK_DGRAM)               #创建AF_INET族的SOCK_DGRAM的套接字
 
-while True:
-    data = input('>>>')
-    data_b = data.encode("UTF-8")
-    if not data_b:
+while True:                                               #发送消息循环
+    data = input('>>>')                                   #获取输入信息
+    data_b = data.encode("UTF-8")                         #转换信息为UTF-8码
+    if not data:                                          #若输入信息为空则停止
         break
-    UDPClientSock.sendto(data_b,ADDR)
-    data_b, ADDR = UDPClientSock.recvfrom(BUFSIZ)
-    data_b = data_b.decode("UTF-8")
-    if not data_b:
+    UDPClientSock.sendto(data_b,ADDR)                     #向ADDR地址（服务器）发送data_b数据报
+    data_b, ADDR = UDPClientSock.recvfrom(BUFSIZ)         #接收信息和地址（ADDR貌似没用了
+    data_b = data_b.decode("UTF-8")                       #对data_解码
+    if not data_b:                                        #若接收的消息为空则停止
         break
-    print(data_b)
+    print(data_b)                                         #打印data_b内容
 
-UDPClientSock.close()
+UDPClientSock.close()                                     #关闭客户端socket
--- Sersock.py
+++ Sersock.py
@@ -1,29 +1,29 @@
 '''   
 这是创建UDP服务器的伪代码
-ss = socket()                           #创建服务器socket
-ss.bind()                               #绑定服务器socket 
-inf_loop:                               #服务器的无限循环
-    cs = ss.recvfrom()/ss.sendto()      #服务器的接收/发送 
-ss.close()                              #关闭服务器（不过好像一般用不到
+ss = socket()                                            #创建服务器socket
+ss.bind()                                                #绑定服务器socket 
+inf_loop:                                                #服务器的无限循环
+    cs = ss.recvfrom()/ss.sendto()                       #服务器的接收/发送 
+ss.close()                                               #关闭服务器（不过好像一般用不到
 '''
-from socket import *
-from time import ctime
+from socket import *                                     #引入模块socket和time
+from time import ctime      
 
-HOST = ''
-PORT = 21567
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+HOST = ''                                                #主机名取任何可用的（对bind的标识
+PORT = 21567                                             #取一个可用端口
+BUFSIZ = 1024                                            #缓存区规定1KB
+ADDR = (HOST, PORT)                                      #服务器地址
 
-UDPSerSock = socket(AF_INET, SOCK_DGRAM)
-UDPSerSock.bind(ADDR)
+UDPSerSock = socket(AF_INET, SOCK_DGRAM)                 #创建服务器的套接字
+UDPSerSock.bind(ADDR)                                    #讲地址绑定到套接字
 
-while True:
-    print('waiting for message...')
-    data, addr = UDPSerSock.recvfrom(BUFSIZ)
-    data_str = data.decode("UTF-8")
-    data_b_time = '[{}]'.format(ctime())
-    data_b_tosend = data_b_time + data_str
-    data_b_tosend = data_b_tosend.encode("UTF-8")
-    UDPSerSock.sendto(data_b_tosend,addr)
-    print('...received from and returned to:',addr)
-UDPSerSock.close()
+while True:                                              #循环开始
+    print('waiting for message...')                      
+    data, addr = UDPSerSock.recvfrom(BUFSIZ)             #获取客户端消息和地址
+    data_str = data.decode("UTF-8")                      #解码消息
+    data_b_time = '[{}]'.format(ctime())                 #添加时间戳data_b_time
+    data_b_tosend = data_b_time + data_str               #拼接字符串
+    data_b_tosend = data_b_tosend.encode("UTF-8")        #编码UTF-8字符串
+    UDPSerSock.sendto(data_b_tosend,addr)                #向客户端地址发送UTF-8编码
+    print('...received from and returned to:',addr)      #打印消息以提示
+UDPSerSock.close()                                       #关闭UDP服务器
